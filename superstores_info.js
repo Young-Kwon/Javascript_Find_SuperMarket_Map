d93afdd9ba5005befb6067e910f88011afd5a98f
YoungKwon <yskwon0415@google.com>
2024-01-24 11:51:00 -0500

    1.0

---
+++ superstores_info.js
@@ -0,0 +1,18 @@
+const places = [
+    { name: "Walmart A, 675 Upper James St, Hamilton, ON L9C 2Z5", type: "walmart", lat: 43.23338399205495, lng: -79.8841812712929 },
+    { name: "Walmart B, 499 Mohawk Rd E, Hamilton, ON L8V 2J3", type: "walmart", lat: 43.222404244263956, lng: -79.8540769240997 },
+    { name: "Walmart C, 1115 Barton St E, Hamilton, ON L8H 2V2", type: "walmart", lat: 43.249984291172424, lng: -79.80038902418033 },
+    { name: "Walmart D, 1051 Garner Rd W, Ancaster, ON L9G 3K9", type: "walmart", lat: 43.197920938316784, lng: -80.0143681327475 },
+    { name: "Walmart E, 2190 Rymal Rd E, Hannon, ON L0R 1P0", type: "walmart", lat: 43.17367609727204, lng: -79.78202649470461 },
+    { name: "Walmart F, 90 Dundas St E, Waterdown, ON L9H 0C2", type: "walmart", lat: 43.31565587973852, lng: -79.90942635324359 },
+    { name: "Canadian Tire A, 11 Clappison Ave, Waterdown, ON L8B 0Y2", type: "canadianTire", lat: 43.322118074023855, lng: -79.91313458291515 },
+    { name: "Canadian Tire B, 119 Osler Dr, Dundas, ON L9H 6X4", type: "canadianTire", lat: 43.265143818408205, lng: -79.9467802119133 },
+    { name: "Canadian Tire C, 304 Main St E, Hamilton, ON L8N 1H9", type: "canadianTire", lat: 43.26014353030771, lng: -79.86026288020373 },
+    { name: "Canadian Tire D, 1283 Barton St E, Hamilton, ON L8H 2V4", type: "canadianTire", lat: 43.25914342340501, lng: -79.8046445955333 },
+    { name: "Canadian Tire E, 777 Upper James St, Hamilton, ON L9C 3A1", type: "canadianTire", lat: 43.23463567284985, lng: -79.88154889038626 },
+    { name: "Canadian Tire F, 686 Queenston Rd, Hamilton, ON L8G 1A3", type: "canadianTire", lat: 43.236636675153505, lng: -79.77511883947369 },
+    { name: "Canadian Tire G, 1060 Wilson St W, Ancaster, ON L9G 3K9", type: "canadianTire", lat: 43.209617600172216, lng: -80.00995159697109 },
+    { name: "Canadian Tire H, 2160 Rymal Rd E, Hannon, ON L0R 1P0", type: "canadianTire", lat: 43.187593204111955, lng: -79.78473187633031 },
+    { name: "Costco A, 100 Legend Ct, Hamilton, ON L9K 1J3", type: "costco", lat: 43.23144643791724, lng: -79.94783163293194 },
+    { name: "Costco B, 1330 S Service Rd, Hamilton, ON L8E 5C5", type: "costco", lat: 43.220439174654175, lng: -79.64090109371102 }
+];
